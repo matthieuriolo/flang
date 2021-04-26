f40869acfe7eec4a084b429697e85335b851df48
Matthieu Riolo <matthieu.riolo@students.ffhs.ch>
2021-04-26 21:45:33 +0200

    implementing proper variable resolver/assignment and possibility for java to communicate with the code

--- flang/src/main/java/ch/ffhs/fac/flang/runtime/Closure.java
+++ flang/src/main/java/ch/ffhs/fac/flang/runtime/Closure.java
@@ -1,11 +1,35 @@
 package ch.ffhs.fac.flang.runtime;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+
+import ch.ffhs.fac.flang.runtime.literals.Function;
+import ch.ffhs.fac.flang.runtime.literals.Identifier;
+import ch.ffhs.fac.flang.runtime.literals.Undefined;
 
 public class Closure {
+	private final Map<String, Literal> variables = new HashMap<String, Literal>();
 	private final Closure parent;
-	private final List<Instruction> instructions = new LinkedList<Instruction>();
+	private final List<Instruction> instructions;
+	
+	@FunctionalInterface
+	public interface FunctionInterface {
+		Literal execute(final Closure closure, final List<Literal> parameters);
+	}
+	
+	private class FunctionBody implements Literal {
+		private final FunctionInterface body;
+		
+		FunctionBody(final FunctionInterface body) {
+			this.body = body;
+		}
+		
+		public Literal functionalCall(final Closure closure, final List<Literal> arguments) {
+			return body.execute(closure, arguments);
+		}
+	}
 	
 	public Closure() {
 		this(null);
@@ -13,6 +37,12 @@ public class Closure {
 	
 	public Closure(final Closure parent) {
 		this.parent = parent;
+		this.instructions = new LinkedList<Instruction>();
+	}
+	
+	public Closure(final Closure parent, final List<Instruction> instructions) {
+		this.parent = parent;
+		this.instructions = instructions;
 	}
 	
 	public void addInstruction(final Instruction instr) {
@@ -23,7 +53,55 @@ public class Closure {
 		return parent;
 	}
 	
-	public void execute(final Context ctx) {
+	public Literal execute(final Closure closure) {
+		for(final var instr : instructions) {
+			final var returnValue = instr.execute(closure);
+			if(returnValue != null) {
+				return returnValue;
+			}
+		}
 		
+		return Undefined.UNDEFINED;
+	}
+	
+	private Closure findClosureWithVariable(final String name) {
+		if(variables.containsKey(name)) {
+			return this;
+		}else if(parent != null) {
+			return parent.findClosureWithVariable(name);
+		}
+		
+		return null;
+	}
+	
+	public Literal getValue(final String name) {
+		final var closure = findClosureWithVariable(name);
+		if(closure != null) {
+			return closure.variables.get(name);
+		}
+		
+		return Undefined.UNDEFINED;
+	}
+	
+	public void setValue(final String name, final Literal value) {
+		final var closure = findClosureWithVariable(name);
+		if(closure != null) {
+			closure.variables.put(name, value);
+		}else {
+			variables.put(name, value);
+		}
+	}
+	
+	public void setValue(final Identifier identifier, final Literal value) {
+		setValue(identifier.getName(), value);
+	}
+	
+	@Deprecated
+	public void declareFunction(final String name, final List<Instruction> instructions) {
+		setValue(name, new Function(List.of(), instructions));
+	}
+	
+	public void declareFunction(final String name, final FunctionInterface body) {
+		setValue(name, new FunctionBody(body));
 	}
 }
