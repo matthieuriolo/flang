87ab41bd756a068151782aa1baf849b471901435
Matthieu Riolo <matthieu.riolo@students.ffhs.ch>
2021-04-27 00:27:06 +0200

    implementing operations for the integer literals

--- flang/src/main/java/ch/ffhs/fac/flang/runtime/literals/Integer.java
+++ flang/src/main/java/ch/ffhs/fac/flang/runtime/literals/Integer.java
@@ -1,11 +1,110 @@
 package ch.ffhs.fac.flang.runtime.literals;
 
+import ch.ffhs.fac.flang.runtime.Literal;
+
 public class Integer extends Number {
 	private final java.lang.Integer integer;
+
 	public Integer(final java.lang.Integer integer) {
 		this.integer = integer;
 	}
-	
+
+	@Override
+	public Literal operationPlus(final Literal right) {
+		if (right instanceof Integer) {
+			return new Integer(integer + ((Integer) right).integer);
+		}
+
+		return super.operationPlus(right);
+	}
+
+	@Override
+	public Literal operationMinus(final Literal right) {
+		if (right instanceof Integer) {
+			return new Integer(integer - ((Integer) right).integer);
+		}
+
+		return super.operationMinus(right);
+	}
+
+	@Override
+	public Literal operationAsterisk(final Literal right) {
+		if (right instanceof Integer) {
+			return new Integer(integer * ((Integer) right).integer);
+		}
+
+		return super.operationAsterisk(right);
+	}
+
+	// TODO: we maybe really need floats :/
+	@Override
+	public Literal operationSlash(final Literal right) {
+		if (right instanceof Integer) {
+			return new Integer(integer / ((Integer) right).integer);
+		}
+
+		return super.operationSlash(right);
+	}
+
+	@Override
+	public Literal operationEqual(final Literal right) {
+		if (right instanceof Integer) {
+			return integer == ((Integer) right).integer ? Boolean.TRUE : Boolean.FALSE;
+		}
+
+		return super.operationEqual(right);
+	}
+
+	@Override
+	public Literal operationNotEqual(final Literal right) {
+		if (right instanceof Integer) {
+			return integer != ((Integer) right).integer ? Boolean.TRUE : Boolean.FALSE;
+		}
+
+		return super.operationNotEqual(right);
+	}
+
+	@Override
+	public Literal operationLess(final Literal right) {
+		if (right instanceof Integer) {
+			return integer < ((Integer) right).integer ? Boolean.TRUE : Boolean.FALSE;
+		}
+
+		return super.operationLess(right);
+	}
+
+	@Override
+	public Literal operationLessEqual(final Literal right) {
+		if (right instanceof Integer) {
+			return integer <= ((Integer) right).integer ? Boolean.TRUE : Boolean.FALSE;
+		}
+
+		return super.operationLessEqual(right);
+	}
+
+	@Override
+	public Literal operationGreater(final Literal right) {
+		if (right instanceof Integer) {
+			return integer > ((Integer) right).integer ? Boolean.TRUE : Boolean.FALSE;
+		}
+
+		return super.operationGreater(right);
+	}
+
+	@Override
+	public Literal operationGreaterEqual(final Literal right) {
+		if (right instanceof Integer) {
+			return integer >= ((Integer) right).integer ? Boolean.TRUE : Boolean.FALSE;
+		}
+
+		return super.operationGreater(right);
+	}
+
+	@Override
+	public Literal operationUnaryMinus() {
+		return new Integer(-integer);
+	}
+
 	@Override
 	public java.lang.String toString() {
 		return java.lang.String.valueOf(integer);
