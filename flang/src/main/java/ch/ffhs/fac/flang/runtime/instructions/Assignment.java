0b23a4acf8a9a7939b6e193e7399ca2dbda61660
Matthieu Riolo <matthieu.riolo@students.ffhs.ch>
2021-04-26 21:47:45 +0200

    implementing instruction assignment

--- flang/src/main/java/ch/ffhs/fac/flang/runtime/instructions/Assignment.java
+++ flang/src/main/java/ch/ffhs/fac/flang/runtime/instructions/Assignment.java
@@ -1,7 +1,24 @@
 package ch.ffhs.fac.flang.runtime.instructions;
 
+import ch.ffhs.fac.flang.runtime.Closure;
+import ch.ffhs.fac.flang.runtime.Expression;
 import ch.ffhs.fac.flang.runtime.Instruction;
+import ch.ffhs.fac.flang.runtime.Literal;
+import ch.ffhs.fac.flang.runtime.literals.Identifier;
 
 public class Assignment implements Instruction {
-
+	private final Identifier identifier;
+	private final Expression expression;
+	
+	public Assignment(final Identifier identifier, final Expression expression) {
+		this.identifier = identifier;
+		this.expression = expression;
+	}
+	
+	@Override
+	public Literal execute(Closure closure) {
+		final var value = expression.compute(closure);
+		closure.setValue(identifier, value);
+		return value;
+	}
 }
