b6baac553e27cb2c7c86aaf52413fd0742501110
Matthieu Riolo <matthieu.riolo@students.ffhs.ch>
2021-04-26 21:05:52 +0200

    made literal identifer self identified

--- flang/src/main/java/ch/ffhs/fac/flang/runtime/literals/Identifier.java
+++ flang/src/main/java/ch/ffhs/fac/flang/runtime/literals/Identifier.java
@@ -4,7 +4,12 @@ import ch.ffhs.fac.flang.runtime.Literal;
 
 public class Identifier implements Literal {
 	private final java.lang.String name;
+	
 	public Identifier(final java.lang.String name) {
 		this.name = name;
+	}
+	
+	public java.lang.String getName() {
+		return name;
 	}
 }
