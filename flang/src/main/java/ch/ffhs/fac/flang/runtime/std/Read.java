639b47d41c61a5c1466e80b84f26d9c4cf8f2546
Matthieu Riolo <matthieu.riolo@students.ffhs.ch>
2021-05-16 02:02:25 +0200

    implementing read() method for reading from console

---
+++ flang/src/main/java/ch/ffhs/fac/flang/runtime/std/Read.java
@@ -0,0 +1,22 @@
+package ch.ffhs.fac.flang.runtime.std;
+
+import java.io.BufferedReader;
+import java.util.List;
+
+import ch.ffhs.fac.flang.runtime.Closure;
+import ch.ffhs.fac.flang.runtime.Literal;
+import ch.ffhs.fac.flang.runtime.Closure.FunctionInterface;
+
+public class Read implements FunctionInterface {
+	public static final String NAME = "read";
+	private final BufferedReader reader;
+
+	public Read(final BufferedReader reader) {
+		this.reader = reader;
+	}
+
+	@Override
+	public Literal execute(Closure closure, List<Literal> parameters) throws Throwable {
+		return new ch.ffhs.fac.flang.runtime.literals.String(reader.readLine());
+	}
+}
