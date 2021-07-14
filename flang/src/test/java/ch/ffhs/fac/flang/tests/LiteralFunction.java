792c28c2e8ae0ae6854c89633a42830e648fcf85
Matthieu Riolo <matthieu.riolo@students.ffhs.ch>
2021-07-14 22:39:50 +0200

    adding tests for functions

---
+++ flang/src/test/java/ch/ffhs/fac/flang/tests/LiteralFunction.java
@@ -0,0 +1,77 @@
+package ch.ffhs.fac.flang.tests;
+
+import org.junit.jupiter.api.Test;
+
+import ch.ffhs.fac.flang.base.TestParserBase;
+
+public class LiteralFunction extends TestParserBase {
+	@Test
+	public void testEnd() throws Throwable {
+		evaluateAssertUndefined("a = :(){"
+				+ "end"
+				+ "}"
+				+ "return a()");
+	}
+	
+	@Test
+	public void testReturn() throws Throwable {
+		evaluateAssertTrue("a = :(){"
+				+ "return true"
+				+ "}"
+				+ "return a()");
+	}
+	
+	@Test
+	public void testScopeInner() throws Throwable {
+		evaluateAssertUndefined("a = :(){"
+				+ "i=1"
+				+ "}"
+				+ "a()"
+				+ "return i");
+	}
+	
+	@Test
+	public void testScopeOuter() throws Throwable {
+		evaluateAssertTrue("b=true"
+				+ " "
+				+ "a=:(){"
+				+ "return b"
+				+ "}"
+				+ "return a()");
+		
+		evaluateAssertTrue("b=false"
+				+ " "
+				+ "a = :(){"
+				+ "b=true"
+				+ "}"
+				+ "a()"
+				+ "return b");
+		
+		evaluateAssertUndefined("a=:(){return c}"
+				+ "b=:(){c=5 return a()}"
+				+ "return b()");
+	}
+	
+	@Test
+	public void testFunctionalProgramming() throws Throwable {
+		evaluateAssertTrue("a = :(){"
+				+ "return :(){return true}"
+				+ "}"
+				+ "return a()()");
+		
+		evaluateAssertTrue("a = :(b){return b()}"
+				+ "return a(:(){return true})");
+	}
+	
+	@Test
+	public void testParameters() throws Throwable {
+		evaluateAssert("a = :(b,c){ return b+c}"
+				+ "return a(1,2)", 3);
+		
+		evaluateAssert("a = :(b,c){ return b+c}"
+				+ "return a(1,2,100)", 3);
+		
+		evaluateAssertUndefined("a = :(b,c){ return b+c}"
+				+ "return a(1)");
+	}
+}
