6fd0c589bf61f35096159226bc082fd23a27a8a5
Matthieu Riolo <matthieu.riolo@students.ffhs.ch>
2021-07-17 15:58:58 +0200

    add JSDoc to newly created exception

--- flang/src/main/java/ch/ffhs/fac/flang/parser/ParserException.java
+++ flang/src/main/java/ch/ffhs/fac/flang/parser/ParserException.java
@@ -2,6 +2,11 @@ package ch.ffhs.fac.flang.parser;
 
 import java.util.List;
 
+/**
+ * Exception for grammar error inside the parser
+ * @author matthieuriolo
+ *
+ */
 public class ParserException extends RuntimeException {
 	private static final long serialVersionUID = -405000998832329228L;
 	final private Location location;
@@ -11,5 +16,21 @@ public class ParserException extends RuntimeException {
 		super("Illegal symbol at " + location.getLine() + ":" + location.getColumn() + " expected " + String.join(", ", values));
 		this.location = location;
 		this.values = values;
+	}
+	
+	/**
+	 * Getter for the location
+	 * @return {@link Location}
+	 */
+	public Location getLocation() {
+		return location;
+	}
+
+	/**
+	 * Getter for the expected tokens
+	 * @return {@link List<String>}
+	 */
+	public List<String> getValues() {
+		return values;
 	}
 }
